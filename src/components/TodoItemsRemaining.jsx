9c8ee1956609a8095ae755306ff0e700dad99b54
Kolodxxv <crowcrxst@gmail.com>
2024-06-10 18:00:29 +0300

    EP-10 - useContext for State Management

--- src/components/TodoItemsRemaining.jsx
+++ src/components/TodoItemsRemaining.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useContext, useMemo } from 'react';
+import { TodosContext } from '../context/TodosContext';
 
-TodoItemsRemaining.propTypes = {
-  remaining: PropTypes.number.isRequired,
-};
+function TodoItemsRemaining() {
+  const { todos } = useContext(TodosContext);
 
-function TodoItemsRemaining(props) {
-  return <div>{props.remaining} items remaining</div>;
+  function remainingCalculation() {
+    // console.log('calculating remaining todos. This is slow');
+    // for (let index = 0; index < 20000000; index++) {}
+    return todos.filter(todo => !todo.isComplete).length;
+  }
+
+  const remaining = useMemo(remainingCalculation, [todos]);
+
+  return <div>{remaining} items remaining</div>;
 }
 
 export default TodoItemsRemaining;
